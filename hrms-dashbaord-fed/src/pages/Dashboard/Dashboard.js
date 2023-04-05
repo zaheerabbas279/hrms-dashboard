498b59a5dc40e4b38227f101c1a8b8362a95d024
zaheerabbas279 <zaheer.a@dollarbirdinc.com>
2023-04-05 12:55:05 +0530

    dynamic side bar

--- hrms-dashbaord-fed/src/pages/Dashboard/Dashboard.js
+++ hrms-dashbaord-fed/src/pages/Dashboard/Dashboard.js
@@ -2,29 +2,44 @@ import React from "react";
 import { Outlet, useNavigate, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
+import TestSidebar from "../../components/Sidebar/testSidebar";
 import { RouteStrings } from "../../utils/common";
 
 const Dashboard = () => {
+  const { isAuth } = useSelector((state) => state.UIStore);
 
-  const { isAuth } = useSelector(state => state.UIStore)
+  const items = [
+    { name: "home", label: "Home" },
+    {
+      name: "billing",
+      label: "Billing",
+      items: [
+        { name: "statements", label: "Statements" },
+        { name: "reports", label: "Reports" },
+      ],
+    },
+    {
+      name: "settings",
+      label: "Settings",
+    },
+  ];
 
-  return (
-    isAuth ?
-      <>
-        <div className="row g-0">
-          <div className="col-md-2">
-            <Sidebar />
-          </div>
-          <div className="col-md-10">
-            <div className="px-3">
-              <Outlet />
-            </div>
+  return isAuth ? (
+    <>
+      <div className="row g-0">
+        <div className="col-md-2">
+          <Sidebar />
+          {/* <TestSidebar items={items} /> */}
+        </div>
+        <div className="col-md-10">
+          <div className="px-3">
+            <Outlet />
           </div>
         </div>
-
-      </>
-      :
-      <Navigate to={RouteStrings.login} />
+      </div>
+    </>
+  ) : (
+    <Navigate to={RouteStrings.login} />
   );
 };
 
