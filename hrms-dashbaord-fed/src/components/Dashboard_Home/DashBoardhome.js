413bfb2ad37852e9ccb7df39a856e3ee36b8c828
zaheerabbas279 <zaheer.a@dollarbirdinc.com>
2023-04-07 13:27:25 +0530

    dashbaord cards fix

--- hrms-dashbaord-fed/src/components/Dashboard_Home/DashBoardhome.js
+++ hrms-dashbaord-fed/src/components/Dashboard_Home/DashBoardhome.js
@@ -11,12 +11,52 @@ export const DashBoardhome = () => {
 
   return (
     <div className="container dashbaord-body my-4">
-      {/* <CreateButton
-                name="Create a new user"
-                handleClick={navigatetoCreateSubAdmin}
-            /> */}
-
       <h4 className="header_color">Dashboard</h4>
+
+      <div className="my-3">
+        <div className="row">
+          <div className="col-md-3">
+            {/* <h5>Organization Chart</h5> */}
+            <div className="card p-3">
+              <h6>Organization Chart</h6>
+
+              <div className="w-100 text-end">
+                <p className="mb-0">
+                  <strong>90</strong>
+                </p>
+                <p className="mb-0">Peers</p>
+              </div>
+            </div>
+          </div>
+          {/* <div className="col-md-3">
+            <div className="card p-3">
+              <h6>Payroll</h6>
+            </div>
+          </div> */}
+          <div className="col-md-3">
+            <div className="card p-3">
+              <h6>Projects</h6>
+              <div className="w-100 text-end">
+                <p className="mb-0">
+                  <strong>0</strong>
+                </p>
+                <p className="mb-0">Projects</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card p-3">
+              <h6>Leaves Count</h6>
+              <div className="w-100 text-end">
+                <p className="mb-0">
+                  <strong>0</strong>
+                </p>
+                <p className="mb-0">Leaves</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
